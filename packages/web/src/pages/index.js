374477d78c4e7352300df0656cb60833af50c988
Matthew Hoth <matthewhoth@gmail.com>
2018-09-20 19:26:19 +0800

    Put the groundwork for cardhome in a separate page

--- packages/web/src/pages/index.js
+++ packages/web/src/pages/index.js
@@ -1,5 +1,6 @@
 import Landing from "./landing/Landing";
 import NotFound from "./notFound/NotFound";
 import ComingSoon from "./comingSoon/ComingSoon";
+import CardHome from "./cards/CardHome";
 
-export { Landing, NotFound, ComingSoon };
+export { Landing, NotFound, ComingSoon, CardHome };
