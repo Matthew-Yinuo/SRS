894136a39b990197b766bbf7ac487228a8e8a94d
Matthew Hoth <matthewhoth@gmail.com>
2018-11-29 16:24:39 +0800

    Reset the yarnworkspace by re-creating react app and going with airtables

--- packages/web/src/modules/register/view/FieldError.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const FieldError = ({ label }: { label: any }) => (
-  <span className="d-block text-danger mt-1">{label}</span>
-);
-
-export default FieldError;
--- packages/web/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Landing from "./landing/Landing";
-import NotFound from "./notFound/NotFound";
-import ComingSoon from "./comingSoon/ComingSoon";
-import CardHome from "./cards/CardHome";
-
-export { Landing, NotFound, ComingSoon, CardHome };
