40943a2cfa9684cdef441ba64c822f31c6cb8735
Matthew Hoth <matthewhoth@gmail.com>
2018-11-29 23:09:56 +0800

    Update the homepage a bit

--- src/app/Cats.js
+++ src/app/Cats.js
@@ -1,27 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import * as api from "./Actions";
-export default class Categories extends React.PureComponent {
-  state = { categories: {} };
+
+export default class Categories extends React.Component {
+  state = { categories: [] };
   componentWillMount() {
     this.fetchCategories();
   }
+
   fetchCategories() {
     api.fetchCategories().then(response => {
       this.setState({ categories: response });
+      console.log(response);
     });
   }
+
   render() {
     const { categories } = this.state;
+
     return (
       <div>
         <div>
-          <h1>Categories</h1>
+          <h1>Study Cards Gamified</h1>
+          <p>Decks of cards for optimized studying</p>
         </div>
         <div>
-          {Object.keys(categories).map((id, key) => (
-            <div key={key}>
-              <Link to={id}>{categories[id]}</Link>
+          {categories.map(category => (
+            <div
+              key={category.id}
+              style={{
+                width: "180px",
+                height: "180px"
+              }}
+            >
+              <Link
+                to={category.id}
+                style={{
+                  fontSize: "14px",
+                  opacity: category.decks ? 1 : 0.65
+                }}
+              >
+                {category.name}
+              </Link>
             </div>
           ))}
         </div>
